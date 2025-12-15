c3123ee802d8586453e191c7a1a1ef813a418df2
CarlosMG06 <220267058+CarlosMG06@users.noreply.github.com>
2025-12-15 17:03:49 +0100

    .

--- JavaFX/Exercici01/src/main/java/com/project/ControllerHello.java
+++ JavaFX/Exercici01/src/main/java/com/project/ControllerHello.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 
-public class ControllerDesktop {
+public class ControllerHello {
 
     @FXML
     private Label labelHello;
